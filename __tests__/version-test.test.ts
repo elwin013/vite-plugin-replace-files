dc5c01e801b00b617dcba9218c62d16cd1a02572
Kamil Banach <kontakt@elwin013.com>
2025-09-11 15:58:55 +0200

    Add vite 7 to peerDependencies, update test

--- __tests__/version-test.test.ts
+++ __tests__/version-test.test.ts
@@ -5,15 +5,18 @@ afterAll(() => {
   execSync(`rm -rf __tests__/vite@*`);
 });
 
-test.each([["vite@3"], ["vite@4"], ["vite@5"], ["vite@6"]])("%s", (version) => {
-  execSync(`cd __tests__ && pnpm dlx ${version} build --outDir ${version}`, {
-    encoding: "utf-8",
-  });
+test.each([["vite@3"], ["vite@4"], ["vite@5"], ["vite@6"], ["vite@7"]])(
+  "%s",
+  (version) => {
+    execSync(`cd __tests__ && pnpm dlx ${version} build --outDir ${version}`, {
+      encoding: "utf-8",
+    });
 
-  var result = execSync(
-    `grep -q \"replacement\" __tests__/${version}/assets/index*; [ $? -eq 0 ] && echo -n \"ok\" || echo -n \"not ok\"`,
-    { encoding: "utf-8" }
-  );
+    var result = execSync(
+      `grep -q \"replacement\" __tests__/${version}/assets/index*; [ $? -eq 0 ] && echo -n \"ok\" || echo -n \"not ok\"`,
+      { encoding: "utf-8" }
+    );
 
-  expect(result).toBe("ok");
-});
+    expect(result).toBe("ok");
+  }
+);
